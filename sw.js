57ff05a6b97f859c41261b9e94f4073f1c3a698f
damtoti28-pixel <damtoti28@gmail.com>
2026-09-10 20:35:34 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'deverrouiller-pdf-v1';
+const CACHE_NAME = 'deverrouiller-pdf-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -9,14 +9,14 @@ const ASSETS = [
   './icon-512.png',
   './favicon-32.png'
 ];
-
+ 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
@@ -25,7 +25,7 @@ self.addEventListener('activate', event => {
   );
   self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cached => cached || fetch(event.request))
